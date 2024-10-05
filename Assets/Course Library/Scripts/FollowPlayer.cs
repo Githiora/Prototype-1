2abffd690002f2401063465e56f488de7c5c28a7
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-10-06 00:53:36 +0300

    camera follows player and toggles between camera using spacebar

--- Assets/Course Library/Scripts/FollowPlayer.cs
+++ Assets/Course Library/Scripts/FollowPlayer.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 8, -11);
-    
+    public GameObject[] cameras;
+
+    private Vector3[] offsets = new Vector3[]
+    {
+        new Vector3(0, 5.43f, -8.73f),
+        new Vector3(0, 2.47f, 0.5f)
+    };
+
+    private int currentCameraIndex = 0;
+    private int offsetIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        // Offset the camera behind the player by adding to the player's position
-        transform.position = player.transform.position + offset;
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cameras[currentCameraIndex].gameObject.SetActive(false);
+
+            currentCameraIndex = (currentCameraIndex + 1) % cameras.Length;
+
+            // Offset the camera behind the player by adding to the player's position
+            offsetIndex = (offsetIndex + 1) % offsets.Length;
+            cameras[currentCameraIndex].gameObject.SetActive(true);
+
+        }
+
+        cameras[currentCameraIndex].gameObject.transform.position = player.transform.position + offsets[offsetIndex];
+        cameras[currentCameraIndex].gameObject.transform.rotation = player.transform.rotation;
+
     }
 }
