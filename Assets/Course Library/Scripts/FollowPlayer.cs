8c6708b995f4ac0740fd1edd4ec5394803f26842
Githiora <gabriel.wamunyu@iconicmoments.co>
2024-11-01 21:39:49 +0300

    clean up

--- Assets/Course Library/Scripts/FollowPlayer.cs
+++ Assets/Course Library/Scripts/FollowPlayer.cs
@@ -18,11 +18,6 @@ public class FollowPlayer : MonoBehaviour
     private int currentCameraIndex = 0;
     private int offsetIndex = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void LateUpdate()
     {
